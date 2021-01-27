9c0e826a5576133d503c6fe7c8e1f6570a8d1e51
nahap <nahappy15@naver.com>
2021-01-27 23:45:57 +0900

    Initial commit

---
+++ src/main/java/com/example/board/dto/BoardDto.java
@@ -0,0 +1,32 @@
+package com.example.board.dto;
+
+import com.example.board.domain.entity.Board;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BoardDto {
+    private Long id;
+    private String author;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public Board toEntity() {
+        Board build = new Board(id, author, title, content);
+        return build;
+    }
+
+    @Builder
+    public BoardDto(Long id, String author, String title, String content, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+}
